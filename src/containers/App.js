50c12103480777246eecb5452b99135711fc55b0
mollyli <mollyli@g.ncu.edu.tw>
2017-02-23 15:41:44 +0800

    add localStorage

--- src/containers/App.js
+++ src/containers/App.js
@@ -20,10 +20,17 @@ if (process.env.NODE_ENV !== 'production') {
   middleware.push(createLogger());
 }
 
+const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {}
+
 const store = createStore(
   reducer,
+  persistedState,
   applyMiddleware(...middleware)
 )
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
 
 store.dispatch(getAllProducts())
 
